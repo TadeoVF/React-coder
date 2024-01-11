cc2211d65fa0be1629d64727036a82a92450015c
TadeoVF <134017910+TadeoVF@users.noreply.github.com>
2024-01-10 22:00:44 -0300

    eh

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css";
+
+const Navbar = () => {
+    return(   
+     <nav>
+    <img src="" alt="logo de la empresa" />
+    <h1>Oscar Herrero</h1>
+    </nav>
+    )
+}
+
+export default Navbar;
